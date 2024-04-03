f0d5d2d3dd7e75acf9055301334d4f60f7cfca3d
Ricardo <ricardinhogiasson16@gmail.com>
2024-04-03 08:27:56 -0300

    SEND FILE

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from "express";
 import QRCode from 'qrcode';
-import { Stream } from "stream";
+import path from 'path';
+
 const router = Router();
 
-router.get('/qr-code', (req: Request, res: Response) => {
+router.get('/qr-code', async (req: Request, res: Response) => {
+  
   const texto: string | undefined = req.query.text as string | undefined;
 
   if (!texto) {
@@ -13,14 +15,36 @@ router.get('/qr-code', (req: Request, res: Response) => {
       });
   }
 
-  // Gerar o QR code
-  QRCode.toFileStream(res, texto)
-      .catch((err: any) => {
+  try {
+    // Gerar o QR code
+    const qrCodeStream = QRCode.toFileStream(texto);
+
+    // Definir o caminho onde o arquivo temporário será salvo
+    const filePath = path.join(__dirname, 'temp', 'qrcode.png');
+
+    // Pipe o stream do QR code para um arquivo temporário
+    qrCodeStream.pipe(res);
+
+    qrCodeStream.on('end', () => {
+      // Enviar o arquivo PNG como resposta
+      res.sendFile(filePath, {}, (err) => {
+        if (err) {
           res.status(500).json({
-              status: false,
-              message: "Erro ao gerar o QR code"
+            status: false,
+            message: "Erro ao enviar o arquivo PNG."
           });
+        } else {
+          // Remover o arquivo temporário após enviar a resposta
+          fs.unlinkSync(filePath);
+        }
       });
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Erro ao gerar o QR code."
+    });
+  }
 });
 
 export default router;
